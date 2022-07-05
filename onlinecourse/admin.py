b532eecc783484a58b0edf93a8ae26e4817cef69
Ramesh Sannareddy <rsannareddy@gmail.com>
2022-07-05 17:42:33 +0000

    changes made from theia lab environment

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-
-
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 5
+    extra = 4
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -21,10 +22,14 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    fields = ('question_text', 'grade', 'lesson_id')
 
 # <HINT> Register Question and Choice models here
-
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
